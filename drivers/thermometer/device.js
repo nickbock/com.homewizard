bec504207121c66b8e822ec70e4631ae4dae8b8e
Freddie Welvering <Freddie@welvering.eu>
2020-07-29 20:18:37 +0200

    small improvements

--- drivers/thermometer/device.js
+++ drivers/thermometer/device.js
@@ -39,7 +39,6 @@ class HomeWizardThermometer extends Homey.Device {
 		}
 
 		// Start polling for thermometer
-
 		refreshIntervalId = setInterval(function () {
 			console.log("--Start Thermometer Polling-- ");
 
@@ -88,56 +87,7 @@ class HomeWizardThermometer extends Homey.Device {
 				});
 			}
 		}
-
-
 	}
-	startPolling2 = function(devices) {
-		if (refreshIntervalId) {
-		  clearInterval(refreshIntervalId);
-		}
-		refreshIntervalId = setInterval(function () {
-			console.log("--Start Thermometer Polling-- ");
-
-			for (var index in devices) {
-
-				if(devices[index].settings.homewizard_id !== undefined ) {
-					var homewizard_id = devices[index].settings.homewizard_id;
-					var thermometer_id = devices[index].settings.thermometer_id;
-					homewizard.getDeviceData(homewizard_id, 'thermometers', function(callback) {
-						if (Object.keys(callback).length > 0) {
-							try {
-								for (var index in callback) {
-
-									if (callback[index].id == thermometer_id) {
-										var te = (callback[index].te.toFixed(1) * 2) / 2;
-										var hu = (callback[index].hu.toFixed(1) * 2) / 2;
-
-										//Check current temperature
-										if (devices[index].getCapabilityValue('measure_temperature') != te) {
-											console.log("New TE - "+ te);
-											devices[index].setCapabilityValue('measure_temperature', te);
-										}
-
-										//Check current temperature
-										if (devices[index].getCapabilityValue('measure_humidity') != hu) {
-											console.log("New HU - "+ hu);
-											devices[index].setCapabilityValue('measure_humidity', hu);
-										}
-									}
-								}
-							} catch (err) {
-								console.log(err);
-								console.log("Thermometer data corrupt");
-							}
-						}
-					});
-				}
-			}
-
-		}, 1000 * 10);
- 	}
-
-
 
 }
 
